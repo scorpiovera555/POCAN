6233cc7cf4afcef7128e38727451c02305fae65a
ikbal129 <kurbaniikbal3@gmail.com>
2026-09-01 07:51:08 +0700

    restart create project

--- Frontend/src/app.jsx
+++ Frontend/src/app.jsx
@@ -1,40 +1,122 @@
-import BottomMenu from "./components/BottomMenu.jsx"
-import Home from "./page/Home.jsx"
-import CanteenProfile from "./page/Profile.jsx"
-import Error from "./page/Error.jsx"
-import { Router, LocationProvider, Route } from "preact-iso";
-import { useEffect, useState, createContext } from "react";
-import CanteenList from "./page/canteenList.jsx";
-import { useFetch } from "./hooks/useFetch.js";
+import { useState } from 'react'
+import heroImg from './assets/hero.png'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import './App.css'
 
-export const ItemsContext = createContext();
-export const UsersContext = createContext();
-
-export function App() {
-  const { data, error } = useFetch("https://dummyjson.com/products");
-  const usersData = useFetch("https://dummyjson.com/users");
-  const user = usersData?.data?.users ?? [];
-  const products = data?.products ?? [];
-
-  if (error) {
-    return <h1>tolong sambungkan ke internet</h1>;
-  }
+function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <LocationProvider>
-      <main>
-        <ItemsContext.Provider value={products}>
-          <UsersContext.Provider value={user}>
-            <Router>
-              <Route path="/" component={() => <Home/>} />
-              <Route path="/kantin/:id" component={() => <CanteenProfile/>} />
-              <Route path="/kantin" component={() => <CanteenList/>} />
-              <Route default component={() => <Error/>} />
-            </Router>
-          </UsersContext.Provider>
-        </ItemsContext.Provider>
-      </main>
-      <BottomMenu />
-    </LocationProvider>
-  );
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+
+      <div className="ticks"></div>
+
+      <section id="next-steps">
+        <div id="docs">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#documentation-icon"></use>
+          </svg>
+          <h2>Documentation</h2>
+          <p>Your questions, answered</p>
+          <ul>
+            <li>
+              <a href="https://vite.dev/" target="_blank">
+                <img className="logo" src={viteLogo} alt="" />
+                Explore Vite
+              </a>
+            </li>
+            <li>
+              <a href="https://react.dev/" target="_blank">
+                <img className="button-icon" src={reactLogo} alt="" />
+                Learn more
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div id="social">
+          <svg className="icon" role="presentation" aria-hidden="true">
+            <use href="/icons.svg#social-icon"></use>
+          </svg>
+          <h2>Connect with us</h2>
+          <p>Join the Vite community</p>
+          <ul>
+            <li>
+              <a href="https://github.com/vitejs/vite" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#github-icon"></use>
+                </svg>
+                GitHub
+              </a>
+            </li>
+            <li>
+              <a href="https://chat.vite.dev/" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#discord-icon"></use>
+                </svg>
+                Discord
+              </a>
+            </li>
+            <li>
+              <a href="https://x.com/vite_js" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#x-icon"></use>
+                </svg>
+                X.com
+              </a>
+            </li>
+            <li>
+              <a href="https://bsky.app/profile/vite.dev" target="_blank">
+                <svg
+                  className="button-icon"
+                  role="presentation"
+                  aria-hidden="true"
+                >
+                  <use href="/icons.svg#bluesky-icon"></use>
+                </svg>
+                Bluesky
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
 }
+
+export default App
--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,8 +1,7 @@
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import preact from '@preact/preset-vite'
-import tailwindcss from "@tailwindcss/vite"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [preact(), tailwindcss()],
+  plugins: [react()],
 })
